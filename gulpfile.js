a4ff81971c06d82a086c0363f68c564669b38841
Leonid <leonid.spb2015@yandex.ru>
2023-02-22 21:27:14 +0300

    Правка верстки

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,43 @@ const { stream } = require("browser-sync");
 const browserSync = require('browser-sync').create();
 const imagemin = require("gulp-imagemin");
 const sourcemaps = require('gulp-sourcemaps');
-
+const pugLint = require('pug-lint');
 
 
 
 
 function compilePug() {
-   return gulp.src("./src/pug/**/*.pug")
-    .pipe(pug({
-            pretty:true
+    return gulp.src("./src/pug/**/*.pug")
+        .pipe(pug({
+            pretty: true
         }))
-    .pipe(gulp.dest('./'))
-    .pipe(browserSync.stream());     
+        .pipe(pugLint)
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
 }
 
 
 
 function CSScompiling() {
     return gulp.src("./src/scss/**/*.scss")
-    .pipe(cleanCSS())
-    .pipe(plumber())    
-    .pipe(sourcemaps.init())
-    .pipe(sass({pretty: true}).on("error", sass.logError))
-    .pipe(autoprefixer())    
-    .pipe(plumber.stop())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/css/')) 
-    .pipe(browserSync.stream());  
+        .pipe(cleanCSS())
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({ pretty: true }).on("error", sass.logError))
+        .pipe(autoprefixer())
+        .pipe(plumber.stop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(browserSync.stream());
 }
 
 
- function script() {
-    return gulp.src("src/js/**/*.js")   
-    .pipe(uglify())
-    .pipe(browserSync.stream())
-    .pipe(gulp.dest('build/js/min.js'));
-} 
+function script() {
+    return gulp.src("src/js/**/*.js")
+        .pipe(uglify())
+        .pipe(browserSync.stream())
+        .pipe(gulp.dest('build/js/min.js'));
+}
 
 
 function liveserver() {
@@ -68,25 +69,26 @@ function watcher() {
     gulp.watch('build/**/*.html').on('change', browserSync.reload);
     gulp.watch('src/img/**/*.{jpg,png,gif,svg}', imageCompressing);
     gulp.watch('src/js', script);
-  }
+}
 
 
 function imageCompressing() {
     return gulp.src([
-        "src/img/**/*.{jpg,png,gif,svg}", 
-        "!src/img/sprites/**/*"])
-    .pipe(imagemin([
-        imagemin.gifsicle({interlaced: true}),
-        imagemin.mozjpeg({quality: 75, progressive: true}),
-        imagemin.optipng({optimizationLevel: 5}),
-        imagemin.svgo({
-            plugins: [ 
-                {removeViewBox: true},
-                {cleanupIDs: false}
-            ]
-        })
-    ]))
-    .pipe(gulp.dest('build/img'));
+            "src/img/**/*.{jpg,png,gif,svg}",
+            "!src/img/sprites/**/*"
+        ])
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('build/img'));
 }
 
 exports.default = gulp.parallel(compilePug, CSScompiling, watcher, imageCompressing);
